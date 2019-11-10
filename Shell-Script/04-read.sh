774074fb4c8a0d0de41cc7419553e580a45bb98c
praveenkanajam <praveenkumar.kanajam@gmail.com>
2019-11-10 18:06:47 +0530

    done

--- Shell-Script/04-read.sh
+++ Shell-Script/04-read.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-## read a value from user input we need to use read command
+## read a value from user input we need to use read command: read name
+read name1
+echo $name1
+
+## read a value from user and with some comment using the command : read -p "comment" variable 
+read -p "Enter your name 2" name2
+echo "Name entered is $name2" 
