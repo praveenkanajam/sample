823fb01c1fc49bf476964e15bfc525d87bacaecd
praveenkanajam <praveenkumar.kanajam@gmail.com>
2019-11-10 14:04:39 +0530

    DONE

--- Shell-Script/02-print.sh
+++ Shell-Script/02-print.sh
@@ -13,4 +13,23 @@ echo -e "Hellow\nWorld"
 echo "tab space"
 echo -e "India\tTelangana\tHyderabad"
 
+## sometimes we need coloures to printed while printing the text to grab the user attention.
 
+
+## syntax for colour enaling : echo -e "\e[COLOURmText" 
+## in the above COLOUR one of the number shown below 
+
+#COLOUR         FORGROUND       BACKGROUND
+#RED                31              41
+#GREEN              32              42
+#YELLOW             33              43
+#BLUE               34              44
+#MAGNETA            35              45
+#CYAN               36              46
+
+echo -e"\e[31mRED"
+echo -e"\e[32mGREEN"
+echo -e"\e[33mYELLOW]
+echo -e"\e[34mYELLOW]
+echo -e"\e[35mMAGNETA]
+echo -e"\e[36mCYAN]
