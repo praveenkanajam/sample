b44fe3e7a39bf458bb944b31f525df68cb94a7aa
praveenkanajam <praveenkumar.kanajam@gmail.com>
2019-11-10 16:38:32 +0530

    done

---
+++ Shell-Script/03-variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## To declare a variable we use VAR=DATA
+            ## EX: a=10,b=red
+## In shell by default their is no data types.
+## a=10,10 is not a number for interpretor, it is just a string.
+## for the beginers it is easy to learn shell, no need to declare the data types.
+## a=true, not a boolian, it is a string
+## a=abc, not a character, but it is string.
+
+## when your data having some special characters then provide input in quotes.. Double is preferred
+
+a=10
+echo a = $a
