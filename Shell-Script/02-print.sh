d953a0fde04b3581cc8197f9dcd4bd05f4b410e3
praveenkanajam <praveenkumar.kanajam@gmail.com>
2019-11-10 10:26:53 +0530

    done

--- Shell-Script/02-print.sh
+++ Shell-Script/02-print.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
 ## to print a message on the screen we user printf comand or echo command
-
 echo hellow world
-printf Hellow WOrld
 
 ## to print a message on multiple lines
 #:  in the below \n is escape sequence which prints new lines.
