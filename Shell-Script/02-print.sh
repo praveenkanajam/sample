9d72542a0317449693cf7b84133ca2eed00572b7
praveenkanajam <praveenkumar.kanajam@gmail.com>
2019-11-10 10:34:46 +0530

    doen

--- Shell-Script/02-print.sh
+++ Shell-Script/02-print.sh
@@ -5,6 +5,7 @@ echo hellow world
 ## to print a message on multiple lines
 #:  in the below \n is escape sequence which prints new lines.
 #    to enable escape sequence we need to use the an option -e to echo command
+#    when you enable the escape sequences in echo command, always provide input in double quotes.
 echo -e "Hellow\nWorld" 
 
 
