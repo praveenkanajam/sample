f12001121e3d39f403b374f809d256781ceecaa6
praveenkanajam <praveenkumar.kanajam@gmail.com>
2019-11-16 11:52:18 +0530

    DONE

--- Teraform/03-provisinors/ec2.tf
+++ Teraform/03-provisinors/ec2.tf
@@ -1,4 +1,18 @@
 resource "aws_instance" "web"{
   ami           = "ami-02383135f96b2293e"
   instance_type = "t2.micro"
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "root"
+      password = "Devops"
+      host     = self.public_ip
+    }
+    inline = [
+      "cd /tmp"
+      "git clone https://gitlab.com/dops42/shell-scripts.git"
+      "puppet apply",
+      "consul join ${aws_instance.web.private_ip}",
+    ]
+  }
 }
