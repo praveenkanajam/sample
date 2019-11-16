03b342e1fd44a65f1b3ba51da614bd7fa50d82ef
praveenkanajam <praveenkumar.kanajam@gmail.com>
2019-11-16 12:27:01 +0530

    done

--- Teraform/03-provisinors/ec2.tf
+++ Teraform/03-provisinors/ec2.tf
@@ -1,12 +1,17 @@
 resource "aws_instance" "web"{
   ami           = "ami-02383135f96b2293e"
   instance_type = "t2.micro"
-  provisioner "remote-exec" {
+  vpc_security_group_ids = ["sg-0611ef56442f01f44","sg-029c586e7bad3d1b1"]
+  provisioner "remote-exec" { // remote-exec it is used to execute the script after instance creation
     connection {
       type     = "ssh"
       user     = "root"
       password = "Devops"
       host     = self.public_ip
+      /* Provisioner Connection Settings
+      Most provisioners require access to the remote resource via SSH or WinRM, and expect a nested connection
+      block with details about how to connect.*/
+
     }
     inline = [
       "cd /tmp",
