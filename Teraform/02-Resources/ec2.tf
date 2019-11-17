55c8f0b1a235aff8a8ab35b6b431a01809b9fb09
praveenkanajam <praveenkumar.kanajam@gmail.com>
2019-11-17 20:50:47 +0530

    done

--- Teraform/02-Resources/ec2.tf
+++ Teraform/02-Resources/ec2.tf
@@ -2,6 +2,10 @@ resource "aws_instance" "web"{
   ami           = "ami-02383135f96b2293e"
   instance_type = "t2.micro"
   provisioner "local-exec" {
-    command = "curl http://checkip.amazonaws.com"
+    inline =[
+      "curl http://checkip.amazonaws.com"
+    ]
+
   }
 }
+
