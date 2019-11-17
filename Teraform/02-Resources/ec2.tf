2185eacaa0e58aabec6ba6dc2d022f53219b5158
praveenkanajam <praveenkumar.kanajam@gmail.com>
2019-11-17 17:25:44 +0530

    --- Auto-Git Commit ---

--- Teraform/02-Resources/ec2.tf
+++ Teraform/02-Resources/ec2.tf
@@ -1,4 +1,7 @@
 resource "aws_instance" "web"{
   ami           = "ami-02383135f96b2293e"
   instance_type = "t2.micro"
+  provisioner "local-exec" {
+    command = "echo This course is ${var.course}"
+  }
 }
