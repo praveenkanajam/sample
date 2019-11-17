22c196e27a8d8b3b04b548292b64dee2f1631510
praveenkanajam <praveenkumar.kanajam@gmail.com>
2019-11-17 08:45:27 +0530

    fonr

--- Teraform/04-variables/02-string/main.tf
+++ Teraform/04-variables/02-string/main.tf
@@ -1,11 +1,8 @@
 variable "sample" {
   default = "Good Morning Hyderabad"
 }
-
-variable "sample1" {
-  default = "Hellow World"
 }
-resource "null_resource" "sample" {
+resource "null_resource" "sample"  {
   provisioner "local-exec" {
     command = "echo ${var.sample}}"
 
