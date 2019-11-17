79ea2de1f29f1c28ad245bb16321486302066afc
praveenkanajam <praveenkumar.kanajam@gmail.com>
2019-11-17 12:33:34 +0530

    done

--- Teraform/04-variables/05-strings/main.tf
+++ Teraform/04-variables/05-strings/main.tf
@@ -1,7 +1,7 @@
 variable "course" {
   default = Devops
 }
-resource "null_resource" "" {
+resource "null_resource" "course" {
 provisioner "local-exec" {
   command = "echo This course is ${var.course}"
 }
