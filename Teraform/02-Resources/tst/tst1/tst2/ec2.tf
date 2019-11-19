910573556e8c8601df0394471635ba33062fc98e
praveenkanajam <praveenkumar.kanajam@gmail.com>
2019-11-19 22:30:44 +0530

    d

--- Teraform/02-Resources/tst/tst1/tst2/ec2.tf
+++ Teraform/02-Resources/tst/tst1/tst2/ec2.tf
@@ -1,10 +1,8 @@
-provider "aws" {
+provider "aws" {}
  resource "aws_instance" "web" {
     ami = "ami-02383135f96b2293e"
     instance_type = "t2.micro"
  tags = {
    Name = "VM111"
+         }
  }
- }
-
-}
