b21b97e77f0dee2b1d86da7ba6b735ee392dbc3a
praveenkanajam <praveenkumar.kanajam@gmail.com>
2019-11-19 20:52:29 +0530

    do

--- Teraform/04-variables/05-maps/maps_old/main.tf
+++ Teraform/04-variables/05-maps/maps_old/main.tf
@@ -6,7 +6,7 @@ variable "maps1" {
     company3 = Oracle
   }
 }
-resource "null_resource" "" {
+resource "null_resource" "map_list" {
   provisioner "local-exec" {
     command = "echo List of Software companys are: ${var.maps1["company1"]} , ${var.maps1["company3"]} and ${var.maps1["company3"]}"
   }
