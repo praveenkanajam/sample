e5b1abcffb4be84032e1a351e5353190d12636b2
praveenkanajam <praveenkumar.kanajam@gmail.com>
2019-11-19 20:50:43 +0530

    done

---
+++ Teraform/04-variables/05-maps/maps_old/main.tf
@@ -0,0 +1,13 @@
+variable "maps1" {
+  type = "map"
+  default = {
+    company1 = Wipro
+    company2 = IBM
+    company3 = Oracle
+  }
+}
+resource "null_resource" "" {
+  provisioner "local-exec" {
+    command = "echo List of Software companys are: ${var.maps1["company1"]} , ${var.maps1["company3"]} and ${var.maps1["company3"]}"
+  }
+}
