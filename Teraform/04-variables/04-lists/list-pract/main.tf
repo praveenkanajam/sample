a9f7486eed9c01eb3e12599cc60e8e9ada280c65
praveenkanajam <praveenkumar.kanajam@gmail.com>
2019-11-20 21:05:37 +0530

    done

--- Teraform/04-variables/04-lists/list-pract/main.tf
+++ Teraform/04-variables/04-lists/list-pract/main.tf
@@ -6,6 +6,6 @@ variable "list" {
 resource "null_resource" "lst" {
   provisioner "local-exec" {
 
-        command = "echo first index is :${var.list["0"]}, Second Index is ${var.list["3"]}"
+        command = "echo first index is :${var.list["0"]}, Last Index is ${var.list["3"]}"
   }
 }
