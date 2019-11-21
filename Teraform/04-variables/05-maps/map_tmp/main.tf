02ac4aadb63ec9d0ce21d8488fc294ba54f0d12a
praveenkanajam <praveenkumar.kanajam@gmail.com>
2019-11-21 20:39:00 +0530

    done

---
+++ Teraform/04-variables/05-maps/map_tmp/main.tf
@@ -0,0 +1,15 @@
+variable "MPP" {
+  type = "map"
+  default = {
+    Fridge = "LG"
+    Board  = "Ikea"
+    Chair  = "Nilkamal"
+  }
+}
+resource "null_resource" "web" {
+  provisioner "local-exec" {
+    command = "echo MY fridge company is :${var.MPP[Fridge]}."
+  }
+}
+
+
