48d96079810ac123065abb87b786063b75dfb75d
praveenkanajam <praveenkumar.kanajam@gmail.com>
2019-11-24 14:55:08 +0530

    done

--- Teraform/03-provisinors/Remote-exec/main.tf
+++ Teraform/03-provisinors/Remote-exec/main.tf
@@ -2,6 +2,7 @@ provider "aws" {}
 resource "aws_instance" "web" {
   ami = "ami-02383135f96b2293e"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0611ef56442f01f44","sg-029c586e7bad3d1b1"]
   tags = {
     Name = "Remote Exec"
   }
