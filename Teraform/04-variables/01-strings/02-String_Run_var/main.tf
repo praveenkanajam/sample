9986d1087d3ac91812e8ec7963a0da9c263d87a7
praveenkanajam <praveenkumar.kanajam@gmail.com>
2019-11-24 12:35:48 +0530

    done

--- Teraform/04-variables/01-strings/02-String_Run_var/main.tf
+++ Teraform/04-variables/01-strings/02-String_Run_var/main.tf
@@ -7,3 +7,14 @@ resource "null_resource" "rn" {
     command = "echo  hello WOrld run time var is ${var.Run_var}}"
   }
 }
+
+/* I/o:
+# terraform apply -auto-approve -var Run_var="Run Time variable"
+null_resource.rn: Creating...
+null_resource.rn: Provisioning with 'local-exec'...
+null_resource.rn (local-exec): Executing: ["/bin/sh" "-c" "echo  hello WOrld run time var is Run Time variable}"]
+null_resource.rn (local-exec): hello WOrld run time var is Run Time variable}
+null_resource.rn: Creation complete after 0s [id=5059577103106956147]
+
+Apply complete! Resources: 1 added, 0 changed, 0 destroyed.
+*/
