04e905a1e336d3a2269bd833cce8b11b258a4fef
praveenkanajam <praveenkumar.kanajam@gmail.com>
2019-11-24 22:40:28 +0530

    do

--- Teraform/05-modules/Test_module/Org-Env/main.tf
+++ Teraform/05-modules/Test_module/Org-Env/main.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "test" {
   ami = var.ami
   instance_type = var.instance_type
-  tags = var.tags
   volume_tags = var.volume_tags
-  }
+  tags = var.tags
 }
--- Teraform/05-modules/Test_module/Prod-Env/main.tf
+++ Teraform/05-modules/Test_module/Prod-Env/main.tf
@@ -2,7 +2,8 @@ module "Dev" {
   source = "../Org-Env"
   ami = var.ami
   instance_type = var.instance_type
-  tags = var.tags
   volume_tags = var.volume_tags
-  }
+  tags = var.tags
+
+}
 
