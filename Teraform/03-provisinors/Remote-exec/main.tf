8a04ae355ce6c0da50a0bc190c8e1dba75cbcc85
praveenkanajam <praveenkumar.kanajam@gmail.com>
2019-11-24 14:30:57 +0530

    done

--- Teraform/03-provisinors/Remote-exec/main.tf
+++ Teraform/03-provisinors/Remote-exec/main.tf
@@ -5,23 +5,24 @@ resource "aws_instance" "web" {
   tags = {
     Name = "Remote Exec"
   }
+}
   provisioner "remote-exec" {
     connection {
-    type = "ssh"
-    username = "root"
-    password = "DevOps321"
-    host = self.public_ip
+      type = "ssh"
+      username = "root"
+      password = "DevOps321"
+      host = self.public_ip
+    }
+    inline = [
+      "echo This is Remote exec inline",
+      "mkdir -p /tmp/remote",
+      "echo $?",
+      "echo IPV4 as below",
+      "ifconfig >> ip.txt",
+      "cat ip.txt",
+      "curl http://checkip.amazonaws.com"
+    ]
   }
-  inline = [
-    "echo This is Remote exec inline",
-    "mkdir -p /tmp/remote",
-    "echo $?",
-    "echo IPV4 as below",
-    "ifconfig >> ip.txt",
-    "cat ip.txt",
-    "curl http://checkip.amazonaws.com"
-  ]
-}
 output "Pub_ip" {
   value = aws_instance.web.public_ip
 }
